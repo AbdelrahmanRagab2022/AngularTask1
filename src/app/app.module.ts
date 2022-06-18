7f4c7a40cd36c0547e0da67e69888f498be80196
AbdelrahmanRagab2022 <abdelrahman.ragab425@gmail.com>
2022-06-18 06:20:08 +0200

    Update Product Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { CacheInterceptor } from './caching.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass:CacheInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
